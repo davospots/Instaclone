0d6025a1d39aae6fc5d4b596664aa3014b16a8f6
Dave <david.mathaga@student.moringaschool.com>
2021-12-07 00:01:49 +0300

    debugging

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 # development
 if config('MODE')=="dev":
    DATABASES = {
